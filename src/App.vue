7fbe4a0273325c3b0fda9ca7441fa99753399f77
marco <lazzari.marco.ml@gmail.com>
2024-02-29 14:02:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  import navbar from './components/navbar.vue';
+
+  export default{
+    components:{
+      navbar,
+    }
+  }
+</script>
+
+<template>
+
+  <navbar></navbar>
+
+</template>
+
+<style lang="scss">
+
+  @use './styles/general' as *;
+
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,70 @@
+<script>
+    export default {
+        name: 'navbar',
+
+        data(){
+            return{
+                links: [
+                    'characters',
+                    'comics',
+                    'movies',
+                    'tv',
+                    'games',
+                    'collectibles',
+                    'videos',
+                    'fans',
+                    'news',
+                    'shop',
+                ]
+            }
+        }
+    }
+
+</script>
+
+<template>
+    <nav>
+        <div class="container">
+            <div class="logo-wrapper">
+                <img src="../../public/img/dc-logo.png" alt="">
+            </div>
+            <div>
+                <ul>
+                    <li v-for="link in links">{{link.toUpperCase()}}</li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss">
+
+    nav{
+        background-color: white;
+
+        .container{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            ul{
+                list-style: none;
+                display: flex;
+
+                li{
+                    padding: 0 0.8rem;
+                }
+            }
+        }
+    }
+
+    .logo-wrapper{
+        max-width: 80px;
+        max-height: 80px;
+
+        img{
+            width: 100%;
+        }
+    }
+
+</style>
